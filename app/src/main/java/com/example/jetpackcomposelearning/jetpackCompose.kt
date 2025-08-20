31d09d668419139868d8199cdbd023c0e7ed26e3
razamasaood <razamasaood.ei@gmail.com>
2025-08-20 17:53:31 +0530

    format code

--- app/src/main/java/com/example/jetpackcomposelearning/jetpackCompose.kt
+++ app/src/main/java/com/example/jetpackcomposelearning/jetpackCompose.kt
@@ -7,7 +7,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -25,7 +24,7 @@ fun increase() {
     Text("$num")
     Button(onClick = {
         num++
-    }){
+    }) {
         Text("Click Me!")
     }
     Button(onClick = {
